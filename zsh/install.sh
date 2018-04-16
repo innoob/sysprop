595e2a9d437e981cbdace8548f07d57598a064e7
InNoob <jb71415@outlook.com>
2018-04-16 16:11:36 +0800

    new change

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+wget https://github.com/InNoob/sysprop/archive/master.zip sysprop.zip
+unzip master.zip
+cd ./master/zsh
 mv ~/.zshrc ~/.zshrc_back
 mv ./zshrc ~/.zshrc
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
